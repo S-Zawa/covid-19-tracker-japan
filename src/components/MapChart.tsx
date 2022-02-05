30249c7208f2a9bb6bfb6b6f82c4509fbae35d41
S.Aizawa <showta4696@gmail.com>
2022-02-06 00:12:17 +0900

    refs #1 日本地図表示とマウスオーバーでツールチップ表示

--- src/components/MapChart.tsx
+++ src/components/MapChart.tsx
@@ -1,68 +1,56 @@
 import React, { memo } from "react";
-import {
-  ZoomableGroup,
-  ComposableMap,
-  Geographies,
-  Geography,
-} from "react-simple-maps";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
-const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-
-const rounded = (num: number) => {
-  if (num > 1000000000) {
-    return Math.round(num / 100000000) / 10 + "Bn";
-  } else if (num > 1000000) {
-    return Math.round(num / 100000) / 10 + "M";
-  } else {
-    return Math.round(num / 100) / 10 + "K";
-  }
-};
+const topoJson = require("../assets/japan.topojson");
 
 const MapChart = ({ setTooltipContent }: { setTooltipContent: any }) => {
   return (
-    <>
-      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
-        <ZoomableGroup>
-          <Geographies geography={geoUrl}>
-            {({ geographies }: { geographies: any }) =>
-              geographies.map(
-                (geo: {
-                  rsmKey: React.Key | null | undefined;
-                  properties: { NAME: any; POP_EST: any };
-                }) => (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    onMouseEnter={() => {
-                      const { NAME, POP_EST } = geo.properties;
-                      setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
-                    }}
-                    onMouseLeave={() => {
-                      setTooltipContent("");
-                    }}
-                    style={{
-                      default: {
-                        fill: "#D6D6DA",
-                        outline: "none",
-                      },
-                      hover: {
-                        fill: "#F53",
-                        outline: "none",
-                      },
-                      pressed: {
-                        fill: "#E42",
-                        outline: "none",
-                      },
-                    }}
-                  />
-                )
+    <div data-tip="">
+      <ComposableMap
+        projection="geoMercator"
+        projectionConfig={{
+          scale: 1200,
+          center: [136.0, 35.6],
+        }}
+      >
+        <Geographies geography={topoJson}>
+          {({ geographies }: { geographies: any }) =>
+            geographies.map(
+              (geo: {
+                rsmKey: React.Key | null | undefined;
+                properties: { nam_ja: any; id: any };
+              }) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    const { nam_ja, id } = geo.properties;
+                    setTooltipContent(`${nam_ja} — ${id}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={{
+                    default: {
+                      fill: "#D6D6DA",
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#F53",
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none",
+                    },
+                  }}
+                />
               )
-            }
-          </Geographies>
-        </ZoomableGroup>
+            )
+          }
+        </Geographies>
       </ComposableMap>
-    </>
+    </div>
   );
 };
 
